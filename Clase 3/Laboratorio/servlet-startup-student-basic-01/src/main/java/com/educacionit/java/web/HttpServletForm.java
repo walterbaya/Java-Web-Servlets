27fbde42025f65e44745198796df3f772a282bed
walterbaya <walterbaya@yahoo.com>
2021-01-09 21:00:12 -0300

    se sube clase 3 de java web

--- Clase 3/Laboratorio/servlet-startup-student-basic-01/src/main/java/com/educacionit/java/web/HttpServletForm.java
+++ Clase 3/Laboratorio/servlet-startup-student-basic-01/src/main/java/com/educacionit/java/web/HttpServletForm.java
@@ -23,40 +23,44 @@ public class HttpServletForm extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
 
-        Map<String, Object> salida = new HashMap();
+        HashMap<String, Object> salida = new HashMap();
         //Captura de datos 
         Map<String, String[]> parametros = request.getParameterMap();
 
-        String nombre = (String)parametros.get("nombre")[0];
+        String nombre = (String) parametros.get("nombre")[0];
         salida.put("nombre", nombre);
-        String apellido = (String)parametros.get("apellido")[0];
+        String apellido = (String) parametros.get("apellido")[0];
         salida.put("apellido", apellido);
-        String dni = (String)parametros.get("dni")[0];
+        String dni = (String) parametros.get("dni")[0];
         salida.put("dni", dni);
-        String direccion = (String)parametros.get("direccion")[0];
+        String direccion = (String) parametros.get("direccion")[0];
         salida.put("direccion", direccion);
-        String ciudad = (String)parametros.get("ciudad")[0];
+        String ciudad = (String) parametros.get("ciudad")[0];
         salida.put("ciudad", ciudad);
-        String telefono = (String)parametros.get("telefono")[0];
+        String telefono = (String) parametros.get("telefono")[0];
         salida.put("telefono", telefono);
-        String email = (String)parametros.get("email")[0];
-         salida.put("email", email);
-        String sexo = (String)parametros.get("sexo")[0];
+        String email = (String) parametros.get("email")[0];
+        salida.put("email", email);
+        String sexo = (String) parametros.get("sexo")[0];
         salida.put("sexo", sexo);
         //Proceso para transformar un String en date
-        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
-        try {
-            Date fechaDeNacimiento = sdf.parse(parametros.get("fechaDeNacimiento")[0]);
-        } catch (ParseException ex) {
-            Logger.getLogger(HttpServletForm.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        salida.put("fechaDeNacimiento", fechaDeNacimiento);
-        boolean graduado = Boolean.parseBoolean((String)parametros.get("nombre")[0]);
+        //SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        //try {
+            String fechaDeNacimiento = (String)parametros.get("fechaDeNacimiento")[0];
+            salida.put("fechaDeNacimiento", fechaDeNacimiento);
+        //} catch (ParseException ex) {
+        //    Logger.getLogger(HttpServletForm.class.getName()).log(Level.SEVERE, null, ex);
+        //}
+        String graduado = ((String) parametros.get("graduado")[0]);
         salida.put("graduado", graduado);
-        String titulo = (String)parametros.get("nombre")[0];
+        String titulo = (String) parametros.get("titulo")[0];
         salida.put("titulo", titulo);
 
         //Guardado de información en el session.
+        
+        request.getSession().setAttribute("salida", salida);
+        
+        response.sendRedirect("index.jsp");
         
     }
 }
--- Clase 3/Laboratorio/servlet-startup-student-basic-01/src/main/webapp/index.jsp
+++ Clase 3/Laboratorio/servlet-startup-student-basic-01/src/main/webapp/index.jsp
@@ -4,6 +4,13 @@
     Author     : WalterAriel
 --%>
 
+<%@page import="java.util.logging.Level"%>
+<%@page import="java.util.logging.Level"%>
+<%@page import="java.text.ParseException"%>
+<%@page import="java.util.Date"%>
+<%@page import="java.text.SimpleDateFormat"%>
+<%@page import="java.util.HashMap"%>
+<%@page import="java.util.Map"%>
 <%@page contentType="text/html" pageEncoding="UTF-8"%>
 <!DOCTYPE html>
 <html>
@@ -11,8 +18,8 @@
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
         <title>JSP Page</title>
     </head>
-     <body>
-        <form method="POST"  action="action">
+    <body>
+        <form method="post"  action="action">
             <label>Nombre: <input type="text" name="nombre"/></label><br>
             <label>Apellido: <input type="text" name="apellido"/></label><br>
             <label>DNI: <input type="text" name="dni"/></label><br> 
@@ -26,8 +33,50 @@
             <label>Titulo: <input type="text" name="titulo"/></label><br>   
             <input type="submit" name="enviar">
         </form>
-        
-        
-        <p>Nombre recibido: </p>
+        <%HashMap<String, Object> entrada = (HashMap<String, Object>) session.getAttribute("salida");
+          String nombre="";
+          String apellido="";
+          String dni="";
+          String direccion="";
+          String ciudad="";
+          String telefono="";
+          String email="";
+          String sexo="";
+          String fechaDeNacimiento="";
+          String graduado="";
+          String titulo="";
+        %>
+        <%if (entrada == null) {%>
+        <p>De momento no se ingreso nada</p>
+        <%} else {%>
+        <p>Se ingresaron datos</p>
+        <%
+            nombre = (String) entrada.get("nombre");
+            apellido = (String) entrada.get("apellido");
+            dni = (String) entrada.get("dni");
+            direccion = (String) entrada.get("direccion");
+            ciudad = (String) entrada.get("ciudad");
+            telefono = (String) entrada.get("telefono");
+            email = (String) entrada.get("email");
+            sexo = (String) entrada.get("sexo");
+            fechaDeNacimiento = (String) entrada.get("fechaDeNacimiento");
+            graduado = (String) entrada.get("graduado");
+            titulo = (String) entrada.get("titulo");
+        %>        
+        <%}%>
+
+        <h2>Informacion de estudiante</h2>
+        <p>El nombre ingresado es: <%=nombre%></p>
+        <p>El apellido ingresado es: <%=apellido%></p>
+        <p>El dni ingresado es: <%=dni%></p>
+        <p>La direccion ingresada es: <%=direccion%></p>
+        <p>La ciudad ingresada es: <%=ciudad%></p>
+        <p>El Telefono ingresado es: <%=telefono%></p>
+        <p>El Email ingresado es: <%=email%></p>
+        <p>El sexo ingresado es: <%=sexo%></p>
+        <p>La fecha de nacimiento ingresada es: <%=fechaDeNacimiento%></p>
+        <p> La persona es graduada: <%=graduado%></p>
+        <p>El titulo ingresado es: <%=titulo%></p>
+
     </body>
 </html>
