c340aeea77c80ae5640102ba4eafc903e2cd9b64
walterbaya <walterbaya@yahoo.com>
2021-07-03 21:22:44 -0300

    se sube modificacion de queryTablaProducto.sql

--- Clase 4/queryTablaProducto.sql
+++ Clase 4/queryTablaProducto.sql
@@ -1,14 +1,8 @@
-create schema demo;
-use demo;
-create table productos(
-id bigint not null auto_increment,
-presentacion varchar(255) not null default '',
-cantidad int(11) not null default 0,
-precio float not null default 0.0,
-descripcion varchar(255),
-primary key(id)
-);
-
-insert into productos(presentacion,cantidad,precio,descripcion) values ('lata durazno', 100, 20.00, 'pack x 10');
-insert into productos(presentacion,cantidad,precio,descripcion) values ('aceite', 120, 50.00, 'pack x 10');
-insert into productos(presentacion,cantidad,precio,descripcion) values ('fideos', 150, 10.00, 'pack x 5');
+use test;
+CREATE TABLE persona(
+	id_persona INT AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(45),
+    apellido VARCHAR(45),
+    email VARCHAR(45),
+    telefono VARCHAR(45)
+) engine=InnoDB
