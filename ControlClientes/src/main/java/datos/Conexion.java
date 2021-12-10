77d233936970d84d5e038c39458cf66e04a919d6
walterbaya <walterbaya@yahoo.com>
2021-12-10 17:16:33 -0300

    se sube conexion a base de datos proyecto de control de clientes

---
+++ ControlClientes/src/main/java/datos/Conexion.java
@@ -0,0 +1,59 @@
+package datos;
+
+import java.sql.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.sql.DataSource;
+import org.apache.commons.dbcp2.BasicDataSource;
+
+/**
+ *
+ * @author Walter
+ */
+public class Conexion {
+
+    //declaramos la jdbcUrl
+    private static final String JDBC_URL = "jdcb:mysql://localhost:3306/control_clientes?useSSL=false&useTimezone=true&serverTimezone=UTC&allowPublicKeyRetrieval=true";
+    private static final String JDBC_USERNAME = "root";
+    private static final String JDBC_PASSWORD = "1234";
+
+    public static DataSource getDataSource() {
+        BasicDataSource ds = new BasicDataSource();
+        ds.setUrl(JDBC_URL);
+        ds.setUsername(JDBC_USERNAME);
+        ds.setPassword(JDBC_PASSWORD);
+        ds.setInitialSize(50);
+        //El pool de conexiones es recomendable crearlo desde el servidor
+        return ds;
+    }
+
+    public static Connection getConnection() throws SQLException {
+        return getDataSource().getConnection();
+    }
+
+    public static void close(ResultSet rs) {
+        try {
+            rs.close();
+        } catch (SQLException ex) {
+            ex.printStackTrace(System.out);
+        }
+    }
+
+    public static void close(PreparedStatement stmt) {
+        try {
+            stmt.close();
+        } catch (SQLException ex) {
+            ex.printStackTrace(System.out);
+        }
+    }
+
+    private static void close(Connection conn) {
+        try {
+            conn.close();
+        } catch (SQLException ex) {
+            Logger.getLogger(Conexion.class.getName()).log(Level.SEVERE, null, ex);
+
+        }
+    }
+
+}
